44e7d0696f2c5bd31527ca353c144d6013cd2596
Li-TianCheng <li_tiancheng666@163.com>
2021-07-03 22:06:59 +0800

    fix: 序列化为字符串

--- utils/include/Serialize.h
+++ utils/include/Serialize.h
@@ -10,66 +10,82 @@
 
 using namespace std;
 
-template<typename T>
-vector<char> serialize(T& obj) {
-    return vector<char>((char*)&obj, (char*)&obj+sizeof(T));
+string strToHex(const vector<unsigned char>& binary) {
+    string s;
+    for (auto& c : binary) {
+        s += (char)((c  >> 4) + 0x30);
+        s += (char)((c & 0x0f) + 0x30);
+    }
+    return s;
 }
 
-template<typename T>
-vector<char> serialize(vector<T>& obj) {
-    return vector<char>((char*)&obj[0], (char*)&obj[0]+obj.size()*sizeof(T));
-}
-
-template<>
-vector<char> serialize(vector<char>& obj) {
-    return obj;
-}
-
-template<typename T>
-vector<char> serialize(list<T>& obj) {
-    vector<char> v;
-    for (auto& c : obj) {
-        v.insert(v.end(), (char*)&c, (char*)&c+sizeof(T));
+vector<unsigned char> hexToStr(const string& str) {
+    vector<unsigned char> v;
+    for (int i = 0; i < str.size(); i+=2){
+        unsigned char left = (unsigned char)str[i] << 4;
+        unsigned char right = ((unsigned char)str[i+1] & 0x0f);
+        v.push_back(left|right);
     }
     return v;
 }
 
-vector<char> serialize(string& obj) {
-    return vector<char>(obj.begin(), obj.end());
+template<typename T>
+string serialize(T& obj) {
+    return strToHex(vector<unsigned char>((unsigned char*)&obj, (unsigned char*)&obj+sizeof(T)));
 }
 
 template<typename T>
-T deserialize(vector<char>& binary){
+string serialize(vector<T>& obj) {
+    return strToHex(vector<unsigned char>((unsigned char*)&obj[0], (unsigned char*)&obj[0]+obj.size()*sizeof(T)));
+}
+
+template<>
+string serialize(vector<unsigned char>& obj) {
+    return string(obj.data(), obj.data()+obj.size());
+}
+
+template<typename T>
+string serialize(list<T>& obj) {
+    vector<unsigned char> v;
+    for (auto& c : obj) {
+        v.insert(v.end(), (unsigned char*)&c, (unsigned char*)&c+sizeof(T));
+    }
+    return strToHex(v);
+}
+
+string serialize(string& obj) {
+    return obj;
+}
+
+template<typename T>
+T deserialize(const string& str){
+    vector<unsigned char> binary = hexToStr(str);
     return *(T*)binary.data();
 }
 
 template<typename T>
-vector<T> deserializeVector(vector<char>& binary) {
+vector<T> deserializeVector(const string& str) {
+    vector<unsigned char> binary = hexToStr(str);
     return vector<T>((T*)&binary[0], (T*)&binary[binary.size()]);
 }
 
 template<>
-vector<char> deserializeVector(vector<char>& binary) {
-    return binary;
+vector<unsigned char> deserializeVector(const string& str) {
+    return hexToStr(str);
 }
 
-string deserializeString(vector<char>& binary) {
-    return binary.data();
+string deserializeString(const string& str) {
+    return str;
 }
 
 template<typename T>
-list<T> deserializeList(vector<char>& binary) {
+list<T> deserializeList(const string& str) {
     list<T> list;
+    vector<unsigned char> binary = hexToStr(str);
     for (int i = 0; i < binary.size(); i+=sizeof(T)) {
         list.push_back(*(T*)&binary[i]);
     }
     return list;
-}
-
-string& appendVector(string& str, vector<char>& binary) {
-    for (auto& b : binary) {
-        str += b;
-    }
 }
 
 #endif //UTILS_SERIALIZE_H
